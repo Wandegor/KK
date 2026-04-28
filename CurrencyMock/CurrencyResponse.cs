50d815b9720e3f399f3c9e60ca27547247e793be
wander122156 <sov48656@mail.ru>
2026-04-28 12:23:44 +0300

    init: сделал Mock для валюты (mountebank)

---
+++ CurrencyMock/CurrencyResponse.cs
@@ -0,0 +1,8 @@
+namespace KK;
+
+public class CurrencyResponse
+{
+    public string Currency { get; set; }
+    public decimal Rate { get; set; }
+    public string Date { get; set; }
+}
---
+++ CurrencyMock/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Net.Http.Json;
+using KK;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        // локальный Mountebank
+        string url = "http://localhost:4545/api/rate";
+        
+        using var client = new HttpClient();
+
+        Console.WriteLine("Запрашиваем курс валют у Mock-сервиса...");
+
+        try 
+        {
+            // Отправляем запрос
+            var response = await client.GetFromJsonAsync<CurrencyResponse>(url);
+
+            if (response != null)
+            {
+                Console.WriteLine("--- Данные получены успешно ---");
+                Console.WriteLine($"Валюта: {response.Currency}");
+                Console.WriteLine($"Курс: {response.Rate} руб.");
+                Console.WriteLine($"Дата обновления: {response.Date}");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка: {ex.Message}");
+        }
+    }
+}
